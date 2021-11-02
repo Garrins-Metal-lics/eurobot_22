cc1b7e409f5c6431d96eebc3bbf545c6bdbae465
ibraamar <amaritona@gmail.com>
2021-11-02 20:11:02 +0100

    Comentari pub

--- navigation/scripts/primer_node.py
+++ navigation/scripts/primer_node.py
@@ -11,6 +11,7 @@ def talker():
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
+        # HPUBLISH MESSAGE
         pub.publish(hello_str)
         rate.sleep()
 
